aacf27dd47c98f15573d25cf21aab3fb32f37b20
isan-san <isan.9.9f@gmail.com>
2022-06-02 23:47:22 -0500

    Integration of sort and filter methods

--- PlayList.java
+++ PlayList.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,107 +23,120 @@ public class PlayList {
         this.playListName = playListName;
     }
 
+    public List<Song> getValues() {
+        return new ArrayList<Song>(this.playList.values());
+    }
+
     public ArrayList<String> getKeys() {
-        Collection<Song> songs = getValues();
+        List<Song> songs = getValues();
         ArrayList<String> songsKeys = new ArrayList<>();
-        for (Song keys : songs) {
-            songsKeys.add(keys.getSongID());
-        }
+        songs.forEach(s -> songsKeys.add(s.getSongID()));
         return songsKeys;
     }
 
-    private Collection<Song> getValues() {
-        return this.playList.values();
-    }
-
     public ArrayList<String> getNames() {
-        Collection<Song> songs = getValues();
-        ArrayList<String> songsNames = new ArrayList<>();
-        for (Song song : songs) {
-            songsNames.add(song.getTitle());
-        }
-        return songsNames;
-
+        List<Song> songs = getValues();
+        ArrayList<String> songNames = new ArrayList<>();
+        songs.forEach(s -> songNames.add(s.getSongID()));
+        return songNames;
     }
 
-    public void sortPlaylist(char sortKey) {
-        // Set<String, Song> set = this.playList.entrySet();
-        // Iterator iterator = set.iterator();
+    public ArrayList<String> getGener() {
+        List<Song> songs = getValues();
+        ArrayList<String> songGeners = new ArrayList<>();
+        songs.forEach(s -> songGeners.add(s.getGenre()));
+        return songGeners;
     }
 
-    public void sortByDate() {
-        LinkedHashMap<Integer, Song> sortByDate = new LinkedHashMap<>();
-        Collection<Song> songs = getValues();
-        for (Song song : songs) {
-            sortByDate.put(song.getDate(), song);
-        }
+    public ArrayList<Integer> getDate() {
+        List<Song> songs = getValues();
+        ArrayList<Integer> songDates = new ArrayList<>();
+        songs.forEach(s -> songDates.add(s.getDate()));
+        return songDates;
     }
 
-    public void sortByLength() {
-        LinkedHashMap<Integer, Song> sortByLength = new LinkedHashMap<>();
-        Collection<Song> songs = getValues();
-        for (Song song : songs) {
-            sortByLength.put(song.getLength(), song);
-        }
-    }
-
-    public Integer getSizeLibrary() {
-        int size = 0;
-        Collection<Song> songs = getValues();
-        for (Song song : songs) {
-            size += 1;
-        }
-        return size;
-    }
-
-    public ArrayList<String> filterGenere() {
-        ArrayList<String> generes = new ArrayList<>();
-        ArrayList<String> generesBySong = new ArrayList<>();
-        Collection<Song> songs = getValues();
-        for (Song song : songs) {
-            generesBySong.add(song.getGenre());
-        }
-        List<String> unique = generesBySong.stream().distinct().collect(Collectors.toList());
-
-        for (int i = 0; i < unique.size(); i++) {
-            System.out.println((i) + " : " + unique.get(i));
-        }
-
-        int indexGenre = in.intInput("Ingrese el indice del genero");
-        String Genre = unique.get(indexGenre);
-        int z = 0;
-        for (Song song : songs) {
-            System.out.println(song.getGenre() + " : " + Genre);
-            if (song.getGenre().equals(Genre)) {
-                generes.add(z+" : "+song.getTitle());
-                z++;
+    public LinkedHashMap<String, Song> filterByGenre() {
+        LinkedHashMap<String, Song> filterdByGenre = new LinkedHashMap<>();
+        ArrayList<String> genresBySong = getGener();
+        List<String> unique = genresBySong.stream().distinct().collect(Collectors.toList());
+        List<Song> songs = getValues();
+        unique.forEach(s -> System.out.println((unique.indexOf(s)) + " : " + s));
+        int indexGenre = in.intInput("Enter the index of the gender you whish to \n filter by (for exaple, enter 0 for "+unique.get(0) +" )");
+        String genre = unique.get(indexGenre);
+        System.out.println("The selected gener is " + genre + "\nThis is the list of " + genre + " in the library");
+        songs.forEach(s -> {
+            if (s.getGenre().equals(genre)) {
+                System.out.println(s.getSongID() + "." + s.getTitle() + " : " + s.getGenre());
+                filterdByGenre.put(s.getSongID(), s);
             }
-        }
-        return generes;
+        });
+        return filterdByGenre;
     }
-    public ArrayList<String> filterYear() {
-        ArrayList<String> years = new ArrayList<>();
-        ArrayList<Integer> yearsBySong = new ArrayList<>();
-        Collection<Song> songs = getValues();
-        for (Song song : songs) {
-            yearsBySong.add(song.getDate());
-        }
-        List<Integer> unique = yearsBySong.stream().distinct().collect(Collectors.toList());
 
-        for (int i = 0; i < unique.size(); i++) {
-            System.out.println((i) + " : " + unique.get(i));
-        }
-
-        int indexGenre = in.intInput("Ingrese el indice del año");
-        int Year = unique.get(indexGenre);
-        int z = 0;
-        for (Song song : songs) {
-            if (song.getDate()==Year) {
-                years.add(z+" : "+song.getTitle());
-                z++;
+    public LinkedHashMap<String, Song> filterByDate() {
+        LinkedHashMap<String, Song> filterdByDate = new LinkedHashMap<>();
+        ArrayList<Integer> datesBySong = getDate();
+        List<Integer> unique = datesBySong.stream().distinct().collect(Collectors.toList());
+        List<Song> songs = getValues();
+        unique.forEach(s -> System.out.println((unique.indexOf(s)) + " : " + s));
+        int indexGenre = in.intInput("Enter the index of the year you whish to\nfilter by (for exaple, enter 0 for "+unique.get(0) +" )");
+        Integer date = unique.get(indexGenre);
+        System.out.println("The selected yaer is " + date + "\nThis is the list of songs relased in " + date + " present in the library");
+        songs.forEach(s -> {
+            if (s.getDate().equals(date)) {
+                System.out.println(s.getSongID() + "." + s.getTitle() + " : " + s.getDate());
+                filterdByDate.put(s.getSongID(), s);
             }
-        }
-        return years;
+        });
+        return filterdByDate;
     }
-    
+
+    /*
+     * public ArrayList<String> filterYear() {
+     * ArrayList<String> years = new ArrayList<>();
+     * ArrayList<Integer> yearsBySong = new ArrayList<>();
+     * Collection<Song> songs = getValues();
+     * for (Song song : songs) {
+     * yearsBySong.add(song.getDate());
+     * }
+     * List<Integer> unique =
+     * yearsBySong.stream().distinct().collect(Collectors.toList());
+     * 
+     * for (int i = 0; i < unique.size(); i++) {
+     * System.out.println((i) + " : " + unique.get(i));
+     * }
+     * 
+     * int indexGenre = in.intInput("Ingrese el indice del año");
+     * int Year = unique.get(indexGenre);
+     * int z = 0;
+     * for (Song song : songs) {
+     * if (song.getDate() == Year) {
+     * years.add(z + " : " + song.getTitle());
+     * z++;
+     * }
+     * }
+     * return years;
+     * }
+     */
+
+    public /* PlayList */void sortByDate() {
+        List<Song> list = new ArrayList<Song>(this.playList.values());
+        list.sort(Comparator.comparingInt(Song::getDate));
+        this.playList.clear();
+        list.forEach(s -> {
+            this.playList.put(s.getSongID(), s);
+        });
+
+    }
+
+    public /* PlayList */void sortByLength() {
+        List<Song> list = new ArrayList<Song>(this.playList.values());
+        list.sort(Comparator.comparingInt(Song::getLength));
+        this.playList.clear();
+        list.forEach(s -> {
+            this.playList.put(s.getSongID(), s);
+        });
+
+    }
+
 }
--- Runing.java
+++ Runing.java
@@ -2,14 +2,24 @@
 public class Runing {
 
     public static void main(String[] args) {
+        
         MainLibrary library = new MainLibrary();
         PlayList mainLibrary = library.createMainLibrary("C:Libro1.csv");
-        System.out.println(mainLibrary.getSizeLibrary());
-
-        for (int x = 0; x < mainLibrary.getSizeLibrary(); x++) {
-            System.out.println(mainLibrary.getKeys().get(x) + " : "+mainLibrary.getNames().get(x));
+        Input in = new Input();
+        //System.out.println(mainLibrary);
+        //mainLibrary.sortByDate();
+        int cont = 0;
+        //mainLibrary.filterByDate();
+        while (cont == 0) {
+            mainLibrary.filterByDate();
+            int cont2 = in.intInput ("If you whant to continue enter anything but 1");
+            if (cont2==1){
+                cont=1;
+            }
         }
-
-        System.out.println(mainLibrary.filterYear());
+        /*for (int x = 0; x < mainLibrary.getSizeLibrary(); x++) {
+            System.out.println(mainLibrary.getKeys().get(x) + " : "+mainLibrary.getNames().get(x));
+        }*/
+        //System.out.println(mainLibrary.filterGenere());
     }
 }
